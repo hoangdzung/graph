d2eb610330fe49b87f3d849fb83f8a56a2d034b7
hoangdzung <dungmin97@gmail.com>
2018-02-01 09:52:02 +0700

    Update train_fisher.py

--- train_fisher.py
+++ train_fisher.py
@@ -73,14 +73,14 @@ def train_model(model, n_epoch):
 			
             # Test phase
             # =======================================================================================
-			total_test_loss = 0.0
-			total_test_softmax_loss =0.0
-			total_test_spatials_loss = 0.0
-			total_test_spatial_loss = 0.0
-			total_test_temporals_loss= 0.0
-			total_test_temporal_loss= 0.0
-            test_accuracy = 0.0
-            batch = 1
+		total_test_loss = 0.0
+		total_test_softmax_loss =0.0
+		total_test_spatials_loss = 0.0
+		total_test_spatial_loss = 0.0
+		total_test_temporals_loss= 0.0
+		total_test_temporal_loss= 0.0
+		test_accuracy = 0.0
+		batch = 1
 
             for i in range(model.queue.num_batch_test):
                 
